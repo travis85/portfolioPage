c1128426b22f580be8a92be6f9472d36c2211960
travis flake <tflake83@gmail.com>
2022-04-11 11:35:20 -0400

    about me section

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function AboutMe() {
+  return (
+    <div className='mt-10 '>
+        <h1 className='flex justify-center ont-mono italic text-blue-900 underline underline-offset-1 text-6xl font-semibold mb-4 '>About Me</h1>
+
+        <div className='flex flex-row '>
+  
+            <div className='flex flex-col max-w-xl text-2xl ml-4  mt-8 basis-1/3'>
+                <br></br>
+                <p>
+                    "There is no greater thing you can do with your life and your work than follow your passions in a way that serves the world and you!"
+                    <br></br> -Ricahard Branson-
+                </p>
+            </div>
+
+            <div className='flex flex-col basis-2/3 place-items-right'>
+                <h1 className=''>I'm Travis, A Junior Software Engineer</h1>
+                <h3>based out of Romulus,Michigan</h3>
+                <p>Enthusiastic fullstack engineer that is not afraid of new challenges! </p><br></br>
+                <p>Hard work + Dedication = Success!</p>
+            </div>
+
+
+        </div>
+        
+        
+
+
+        
+
+        
+
+
+    </div>
+  )
+}

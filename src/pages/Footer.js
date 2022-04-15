f9da0910228589d5b715d04f74977cdb28e13412
travis flake <tflake83@gmail.com>
2022-04-15 10:52:24 -0400

    weather app link

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -3,14 +3,16 @@ import weatherPic from '../assets/weatherPic.jpeg'
 import toDoListPic from '../assets/toDoListPic.png'
 import scrabblePic from '../assets/scrabblePic.jpeg'
 
+export default function Footer() {
+    
+    
+    
 
-export default function Footer(){
     return(
         
         <div className=" mt-20  space-y-20" >
-
             <div className="flex justify-center text-blue-500 text-6xl  font-semibold mb-4 ">
-                <h1 className= "font-mono italic text-blue-900 underline underline-offset-1	" >Featured Projects</h1>
+                <h1 className= "font-mono italic text-blue-900 underline underline-offset-1" >Featured Projects</h1>
             </div>
             <div className="card mb-3 shadow-xl mr-[20%] ml-2 rounded-xl bg-gradient-to-r from-blue-500 to-transparent" >
                 <div className="row g-0">
@@ -20,9 +22,9 @@ export default function Footer(){
                     <div className="col-md-8 ">
 
                         <div className="card-body">
-                            <a href="https://github.com/travis85/weatherApp"><h5 className=" flex justify-center italic font-semibold text-4xl hover:-translate-y-1 hover:scale-110 hover:animate-bounce text-blue-500">Weather App</h5></a>
+                            <a href="weather-app-4a90b.web.app"><h5 className=" flex justify-center italic font-semibold text-4xl hover:-translate-y-1 hover:scale-110 hover:animate-bounce text-blue-500">Weather App</h5></a>
                             <div className="flex justify-center mt-20" >
-                            <p className="text-2xl">Find out what's the weather like in your favorite City!</p>
+                                <p className="text-2xl">Find out what's the weather like in your favorite City!</p>
 
                             </div>
                          </div>
@@ -31,11 +33,13 @@ export default function Footer(){
             </div>
 
             <div className="card mb-3 shadow-xl ml-[20%] mr-2 rounded-xl bg-gradient-to-l from-blue-500 to-transparent" >
+                
                 <div className="float-right ">
                     <div className="col-md-4 float-right">
                         <img src={ scrabblePic } className="rounded-xl h-80" alt="..."/>
                     </div>
                     <div className="col-md-8">
+                    
                         <div className="card-body">
                             <a href="https://github.com/travis85/scrabbleSolverInterview/tree/main"><h1 className="flex justify-center italic font-semibold text-4xl hover:-translate-y-1 hover:scale-110 hover:animate-bounce text-blue-500">Scrabble Cheater</h1></a>
                             <div className="flex justify-center mt-20 text-2xl">
